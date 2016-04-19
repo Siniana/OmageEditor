dda0bf86c6be5335d6108ac40a29551a636829a3
Alina Böttcher <alina.boettcher@freenet.de>
2016-04-19 15:53:50 +0200

    fight raus

--- Fight.java
+++
@@ -1,6 +0,0 @@
-public Fight{
-	public fight(){
-		
-	}
-
-}
--- main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-	public static void main(String[] args){
-		Dragon dragon = new Dragon();
-		Fire fire = new Fire();
-	}
-}
